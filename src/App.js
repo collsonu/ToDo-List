90219bcc30177971b8095c8b22a68fe58c9dc155
sonu kumar <collsonu.kumar@gmail.com>
2021-12-03 13:57:52 +0530

    adding todo list

--- src/App.js
+++ src/App.js
@@ -1,25 +1,71 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
+import React, { useState } from "react";
+import ToDoList from "./ToDoList";
+
+const App = () => {
+  const [items, setItems] = useState("");
+  const [showItems, setShowItems] = useState([]);
+
+  const ItemEvent = (e) => {
+    setItems(e.target.value);
+  };
+  // const listOfItems = () => {
+
+  // };
+  const deleteItem = (id) => {
+    setShowItems((oldData) => {
+      return oldData.filter((arrElem, index) => {
+        return index !== id;
+      });
+    });
+  };
+  // setShowItems((oldData) => {
+  //   return oldData.filter((arrElem, index)=>{
+  //     return index !==id;
+  //   })
+  // })
+
+  const formSubmitHandler = (e) => {
+    e.preventDefault();
+    setShowItems((oldData) => {
+      return [...oldData, items];
+    });
+    setItems("");
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+    <>
+      <div className="main_div">
+        <div className="center_div">
+          <br />
+          <h1>ToDo List</h1>
+          <br />
+          <form className="form-container" onSubmit={formSubmitHandler}>
+            <input
+              type="text"
+              placeholder="Add a Items"
+              value={items}
+              onChange={ItemEvent}
+            />
+            <button type="submit">+</button>
+          </form>
 
+          <ol>
+            {/* <li>{items}</li> */}
+            {showItems.map((itemvalue, index) => {
+              return (
+                <ToDoList
+                  key={index}
+                  id={index}
+                  text={itemvalue}
+                  onSelect={deleteItem}
+                />
+              );
+            })}
+          </ol>
+        </div>
+      </div>
+    </>
+  );
+};
 export default App;
